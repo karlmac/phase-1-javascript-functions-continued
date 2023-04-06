59a495bc19b6e89c6fa3968b29e4229a292ede04
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-04-05 23:32:20 -0500

    edits index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+//const wrapAdjective = 
+function wrapAdjective(str1 = "*") {
+    return function (str2 = "special") {
+        return `You are ${str1}${str2}${str1}!`;
+    }
+}
+
+//console.log(wrapAdjective("#")("a dedicated programmer"));
+
